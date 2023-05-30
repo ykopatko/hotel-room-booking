5e6f547883ef77815336bdb97cb40d268ca77c3e
Yevhen Kopatko <cs.kopatko.evgen@gmail.com>
2023-05-30 14:28:10 +0300

    Create models in sql_app/models.py

---
+++ sql_app/models.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+
+from sqlalchemy import Integer, Column, String, DateTime, ForeignKey
+from sql_app.database import Base
+from sqlalchemy.orm import relationship
+
+
+class DBCategory(Base):
+    __tablename__ = "categories"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(255), unique=True, nullable=False)
+    description = Column(String(511))
+
+    products = relationship("DBProduct", back_populates="category")
+
+
+class DBProduct(Base):
+    __tablename__ = "products"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(255), unique=True, nullable=False)
+    description = Column(String(511), nullable=False)
+    price = Column(Integer, nullable=False)
+    quantity = Column(Integer, nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow,  nullable=False)
+    category_id = Column(Integer, ForeignKey("categories.id"))
+
+    category = relationship("DBCategory", back_populates="products")
